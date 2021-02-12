901b9aba984be97b803cae2f178f4ee74e2cb627
Steffen Jahr <steffen.jahr@thinktecture.com>
2021-02-12 07:57:44 +0100

    chore: empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Inject} from '@angular/core';
 import {PaintService} from './paint.service';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -7,33 +8,21 @@ import {PaintService} from './paint.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild('canvas')
-  canvas: ElementRef<HTMLCanvasElement>;
+  // EX #1
 
-  context: CanvasRenderingContext2D;
+  // EX #11
+
+
   previousPoint: { x: number, y: number } | null = null;
-  isClipboardDisabled = !('clipboard' in navigator && 'read' in navigator.clipboard);
-  isShareDisabled = !('canShare' in navigator);
+  // EX #17
 
-  constructor(private paintService: PaintService) {
+  constructor(private paintService: PaintService, @Inject(DOCUMENT) private document: Document) {
   }
 
   async ngAfterViewInit(): Promise<any> {
-    const ctx = this.context = this.canvas.nativeElement.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    ctx.fillStyle = 'black';
+    // EX #2
 
-    if ('launchQueue' in window) {
-      (window as any).launchQueue.setConsumer(async params => {
-        const [handle] = params.files;
-        if (handle) {
-          const file = await handle.getFile();
-          const image = await this.paintService.getImage(file);
-          ctx.drawImage(image, 0, 0);
-        }
-      });
-    }
+    // EX #16
   }
 
   onPointerDown(event: PointerEvent): void {
@@ -41,16 +30,7 @@ export class AppComponent implements AfterViewInit {
   }
 
   onPointerMove(canvas: HTMLCanvasElement, event: PointerEvent): void {
-    if (this.previousPoint) {
-      const currentPoint = {x: Math.floor(event.offsetX), y: Math.floor(event.offsetY)};
-      for (const {
-        x,
-        y
-      } of this.paintService.bresenhamLine(this.previousPoint.x, this.previousPoint.y, currentPoint.x, currentPoint.y)) {
-        this.context.fillRect(x, y, 2, 2);
-        this.previousPoint = currentPoint;
-      }
-    }
+    // EX #4
   }
 
   onPointerUp(): void {
@@ -58,50 +38,28 @@ export class AppComponent implements AfterViewInit {
   }
 
   colorChange($event) {
-    this.context.fillStyle = $event.target.value;
+    // EX #6
+
   }
 
   async open(): Promise<void> {
-    const [handle] = await (window as any).showOpenFilePicker();
-    const file = await handle.getFile();
-    const image = await this.paintService.getImage(file);
-    this.context.drawImage(image, 0, 0);
+    // EX #12
   }
 
   async save(): Promise<void> {
-    const blob = await this.paintService.toBlob(this.canvas.nativeElement);
-    const handle = await (window as any).showSaveFilePicker();
-    const writable = await handle.createWritable();
-    await writable.write(blob);
-    await writable.close();
+    // EX #11
+    // EX #18
   }
 
   async copy(): Promise<void> {
-    const blob = await this.paintService.toBlob(this.canvas.nativeElement);
-    await navigator.clipboard.write([
-      new ClipboardItem({[blob.type]: blob})
-    ]);
+    // EX #13
   }
 
   async paste(): Promise<void> {
-    const clipboardItems = await navigator.clipboard.read();
-    for (const clipboardItem of clipboardItems) {
-      for (const type of clipboardItem.types) {
-        if (type === 'image/png') {
-          const blob = await clipboardItem.getType(type);
-          const image = await this.paintService.getImage(blob);
-          this.context.drawImage(image, 0, 0);
-        }
-      }
-    }
+    // EX #14
   }
 
   async share(): Promise<any> {
-    const blob = await this.paintService.toBlob(this.canvas.nativeElement);
-    const file = new File([blob], 'untitled.png', {type: 'image/png'});
-    const item = {files: [file], title: 'untitled.png'};
-    if (await navigator.canShare(item)) {
-      await navigator.share(item);
-    }
+    // EX #15
   }
 }
